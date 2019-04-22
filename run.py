8401f3dc31011ebb7395f6f843617f9aeb3a3914
jojo <jaxlouder@gmail.com>
2019-04-22 15:13:49 +0800

    2019/04/22

--- run.py
+++ run.py
@@ -1,14 +1,35 @@
 # -*- coding:utf-8 -*- 
-
-import jieba
-import db_tool as DB
+from flask import render_template
 from jieba.analyse import *
 from flask import Flask
-import itertools
 import urllib.parse
+import itertools
+import sqlite3
+import jieba
 
 app = Flask(__name__) # 應用程式初始化
 
+# 定義資料庫物件
+def get_db(keyword):
+    k = ('%' + keyword + '%',)
+    try:
+        db_keyword_str = ""
+        DB = sqlite3.connect('keyword.db')
+        cur = DB.execute('''SELECT * FROM
+                                keyword_table 
+                                WHERE keyword LIKE (?) ''', k)
+        rows = cur.fetchall()
+        for row in rows:
+            db_keyword_str += str(row[0]) + ","
+            DB.close()
+
+        return db_keyword_str
+
+    except Exception as err:
+
+        return err
+
+# 測試是否正常啟動
 @app.route('/')
 def start():
     talk = '啟動成功'
@@ -19,15 +40,15 @@ def start():
 def show_user_profile(article):
     
     #### 解碼成中文字串 ####
-    chinese_word = urllib.parse.unquote(article).replace('未經許可，禁止轉載責任編輯：',"")
-    chinese_word = chinese_word.replace('.',"").replace('+','').replace('0','').replace('_','')
+    # chinese_word = urllib.parse.unquote(article).replace('未經許可，禁止轉載責任編輯：',"")
+    chinese_word = article.replace('.',"").replace('+','').replace('0','').replace('_','').replace('未經許可，禁止轉載責任編輯：',"")
 
     #### 載入自訂義關鍵字 ####
-    file_name = './suggest_keyword.txt'
+    file_name = 'suggest_keyword.txt'
     jieba.load_userdict(file_name) 
 
     #### 載入停用關鍵字 #####
-    with open("./stop_words.txt","r",encoding='utf-8') as f:
+    with open("stop_words.txt","r",encoding='utf-8') as f:
         stop_word = f.read().replace("\ufeff","").split(",")
         
     #### 文章斷詞 ####
@@ -46,29 +67,35 @@ def show_user_profile(article):
     for k,w in extract_tags(article,topK=4,withWeight=True):
         top4_Word_ls += [k]
 
-    ## 重新做關鍵字的排列組合
-    k1,k2,k3,k4 = top4_Word_ls[0],top4_Word_ls[1],top4_Word_ls[2],top4_Word_ls[3] 
+    # 重新做關鍵字的排列組合
+    k1,k2,k3,k4 = top4_Word_ls[0],top4_Word_ls[1],top4_Word_ls[2],top4_Word_ls[3]
     k_ls = list(itertools.permutations([k1,k2,k3,k4],2))
     word_ls = [] # 12種排列組合的關鍵字
     for i in k_ls:
-        word_ls += [" ".join(i)]
+        word_ls += ["".join(i)]
 
     word16_ls = word_ls + top4_Word_ls # 16種排列組合的關鍵字
 
-    ### 從資料庫撈出文章關鍵字 ###
+    ## 從資料庫撈出文章關鍵字 ###
+    keyWords_ls = []
     for keyword in word16_ls:
-        key_ls = DB.get_data(keyword)
+        uListStr = get_db(keyword)
+        keyWords_ls += [uListStr]
 
-    # 小於or等於10個tag , 則回傳10個tag
-    if len(key_ls) <= 10:
-        keyword_str = ",".join(key_ls)
-        
-    # 大於10個tag , 則回傳10個tag 
-    elif len(key_ls) > 10:
-        keyword_str = ",".join(key_ls[:11])
+    keyWords = ",".join(keyWords_ls).split(",")
+    result = [i for i in keyWords if i not in [""]]
+    
+    # 若關鍵字如果數量超過10個，則先取15個，再去重複
+    if len(result) > 10:
+        result = list(set(result[:15]))
+        result_str = ",".join(result[:10])
+    
+    # 若關鍵字數量小於等於10個則直接輸出
+    elif len(result) <= 10:
+        result_str = ",".join(result)
 
-    return keyword_str
+    return result_str
 
-app.run(debug=False) # 正式運作api時 , 請調整回debug=false
-                     # port=5500 可以依情況修改, 啟動網址 → http://127.0.0.1:5500(本機端執行，無法從外部拜訪)
+app.run(debug=True,port=5000) # 正式運作api時 , 請調整回debug=false
+                     # port=5000 可以依情況修改, 啟動網址 → http://127.0.0.1:5000(本機端執行，無法從外部拜訪)
 
