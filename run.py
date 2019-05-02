ca8abf8a950a25b93ee18829925e5a90fbef0a08
jojo <jaxlouder@gmail.com>
2019-05-02 12:54:33 +0800

    2019/05/02

--- run.py
+++ run.py
@@ -10,6 +10,7 @@ import itertools
 import sqlite3
 import config
 import jieba
+import re
 
 app = Flask(__name__) # 應用程式初始化
 app.config.from_object(config)
@@ -95,15 +96,21 @@ def submit():
             # 重新做關鍵字的排列組合
             k1,k2,k3,k4 = top4_Word_ls[0],top4_Word_ls[1],top4_Word_ls[2],top4_Word_ls[3]
             k_ls = list(itertools.permutations([k1,k2,k3,k4],2))
-            word_ls = [] # 12種排列組合的關鍵字
-            for i in k_ls:
-                word_ls += ["".join(i)]
 
-            word16_ls = word_ls + top4_Word_ls # 16種排列組合的關鍵字
-            
+            # 模糊比對關鍵字是否存在文章
+            exist_string_ls = [] 
+            for j in k_ls:
+                a,b = j[0],j[1]
+                m = re.match('.*' + a + '.*' + b, article)
+                if m != None:
+                    s = a+b
+                    exist_string_ls += [s]
+
+            exist_string_ls = exist_string_ls + top4_Word_ls # 加入高權重4組詞彙
+
             ## 從資料庫撈出文章關鍵字 ###
             keyWords_ls = []
-            for keyword in word16_ls:
+            for keyword in exist_string_ls:
                 db_keyword_str = get_db(keyword)
                 keyWords_ls += [db_keyword_str]
             
@@ -112,8 +119,8 @@ def submit():
             
             # 若關鍵字如果數量超過10個，則先取15個，再去重複
             if len(result) > 10:
-                result = list(set(result[:15]))
-                data = result[:10]
+                result = list(set(result[:11]))
+                data = result
                 status = "true"
                 msg = "success"
                 result_str = json_format(data,status,msg)
@@ -133,9 +140,8 @@ def submit():
 
             return result_str
 
-        except:
+        except Exception as status:
             data = "None"
-            status = "false" 
             msg = "或許您的文章長度不夠，導致無法成功解析關鍵字","或許資料庫無此文章關鍵字，導致無法回傳關鍵字"
             result_str = json_format(data,status,msg)
 
@@ -144,69 +150,68 @@ def submit():
     return render_template("post_submit.html")
 
 # 如果想將arg 傳入所定義好的 function,可以使用以下方法 
-@app.route('/article/<article>')
-def show_user_profile(article):
+# @app.route('/article/<article>')
+# def show_user_profile(article):
     
-    #### 解碼成中文字串 ####
-    # chinese_word = urllib.parse.unquote(article).replace('未經許可，禁止轉載責任編輯：',"")
-    chinese_word = article.replace('.',"").replace('+','').replace('0','').replace('_','').replace('未經許可，禁止轉載責任編輯：',"")
+#     #### 解碼成中文字串 ####
+#     # chinese_word = urllib.parse.unquote(article).replace('未經許可，禁止轉載責任編輯：',"")
+#     chinese_word = article.replace('.',"").replace('+','').replace('0','').replace('_','').replace('未經許可，禁止轉載責任編輯：',"")
 
-    #### 載入自訂義關鍵字 ####
-    # file_name = '/home/cfd888/external_hdd/public_html/temp.check-article.cfd888.info/suggest_keyword.txt'
-    file_name = 'suggest_keyword.txt'
-    jieba.load_userdict(file_name) 
+#     #### 載入自訂義關鍵字 ####
+#     file_name = '/home/cfd888/external_hdd/public_html/temp.check-article.cfd888.info/suggest_keyword.txt'
+#     # file_name = 'suggest_keyword.txt'
+#     jieba.load_userdict(file_name) 
 
-    #### 載入停用關鍵字 #####
-    # stop_word_path = "/home/cfd888/external_hdd/public_html/temp.check-article.cfd888.info/stop_words.txt"
-    stop_word_path = "stop_words.txt"
-    with open(stop_word_path,"r",encoding='utf-8') as f:
-        stop_word = f.read().replace("\ufeff","").split(",")
+#     #### 載入停用關鍵字 #####
+#     stop_word_path = "/home/cfd888/external_hdd/public_html/temp.check-article.cfd888.info/stop_words.txt"
+#     # stop_word_path = "stop_words.txt"
+#     with open(stop_word_path,"r",encoding='utf-8') as f:
+#         stop_word = f.read().replace("\ufeff","").split(",")
         
-    #### 文章斷詞 ####
-    seg_list = jieba.cut_for_search(chinese_word, HMM=True) # 搜尋引擎模式
-    content = "/".join(seg_list)
+#     #### 文章斷詞 ####
+#     seg_list = jieba.cut_for_search(chinese_word, HMM=True) # 搜尋引擎模式
+#     content = "/".join(seg_list)
 
-    content_ls = content.split("/")
+#     content_ls = content.split("/")
 
-    ### 清理冗詞 ###
-    correct_word = [ w for w in content_ls if w not in stop_word ]
-    article = ",".join(correct_word)
+#     ### 清理冗詞 ###
+#     correct_word = [ w for w in content_ls if w not in stop_word ]
+#     article = ",".join(correct_word)
 
-    top4_Word_ls = []
+#     top4_Word_ls = []
 
-    # 關鍵字權重
-    for k,w in extract_tags(article,topK=4,withWeight=True):
-        top4_Word_ls += [k]
+#     # 關鍵字權重
+#     for k,w in extract_tags(article,topK=4,withWeight=True):
+#         top4_Word_ls += [k]
 
-    # 重新做關鍵字的排列組合
-    k1,k2,k3,k4 = top4_Word_ls[0],top4_Word_ls[1],top4_Word_ls[2],top4_Word_ls[3]
-    k_ls = list(itertools.permutations([k1,k2,k3,k4],2))
-    word_ls = [] # 12種排列組合的關鍵字
-    for i in k_ls:
-        word_ls += ["".join(i)]
+#     # 重新做關鍵字的排列組合
+#     k1,k2,k3,k4 = top4_Word_ls[0],top4_Word_ls[1],top4_Word_ls[2],top4_Word_ls[3]
+#     k_ls = list(itertools.permutations([k1,k2,k3,k4],2))
+#     word_ls = [] # 12種排列組合的關鍵字
+#     for i in k_ls:
+#         word_ls += ["".join(i)]
 
-    word16_ls = word_ls + top4_Word_ls # 16種排列組合的關鍵字
+#     word16_ls = word_ls + top4_Word_ls # 16種排列組合的關鍵字
     
-    ## 從資料庫撈出文章關鍵字 ###
-    keyWords_ls = []
-    for keyword in word16_ls:
-        db_keyword_str = get_db(keyword)
-        keyWords_ls += [db_keyword_str]
+#     ## 從資料庫撈出文章關鍵字 ###
+#     keyWords_ls = []
+#     for keyword in word16_ls:
+#         db_keyword_str = get_db(keyword)
+#         keyWords_ls += [db_keyword_str]
     
-    keyWords = ",".join(keyWords_ls).split(",")
-    result = [i for i in keyWords if i not in [""]]
+#     keyWords = ",".join(keyWords_ls).split(",")
+#     result = [i for i in keyWords if i not in [""]]
     
-    # 若關鍵字如果數量超過10個，則先取15個，再去重複
-    if len(result) > 10:
-        result = list(set(result[:15]))
-        result_str = ",".join(result[:10])
+#     # 若關鍵字如果數量超過10個，則先取15個，再去重複
+#     if len(result) > 10:
+#         result = list(set(result[:15]))
+#         result_str = ",".join(result[:10])
     
-    # 若關鍵字數量小於等於10個則直接輸出
-    elif len(result) <= 10:
-        result_str = ",".join(result)
+#     # 若關鍵字數量小於等於10個則直接輸出
+#     elif len(result) <= 10:
+#         result_str = ",".join(result)
 
-    return result_str
+#     return result_str
 
 app.run(debug=True,port=5000) # 正式運作api時 , 請調整回debug=false
-                     # port=5000 可以依情況修改, 啟動網址 → http://127.0.0.1:5000(本機端執行，無法從外部拜訪)
-
+                              # port=5000 可以依情況修改, 啟動網址 → http://127.0.0.1:5000(本機端執行，無法從外部拜訪)
